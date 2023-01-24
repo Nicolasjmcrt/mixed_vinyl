f380f306c4e620b6aaf7873d7e4b7b667bbba94a
Nicolasjmcrt <jumeaucourtn@gmail.com>
2023-01-24 17:21:10 +0100

    Chapter 12 : JSON API Endpoint

---
+++ src/Controller/SongController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class SongController extends AbstractController {
+
+    #[Route('/api/song/{id}', name: 'app_song')]
+    public function getSong($id): Response
+    {
+        // TODO: query the database
+        $song = [
+            'id' => $id,
+            'name' => 'Waterfalls',
+            'url' => 'https://symfonycasts.s3.amazonaws.com/sample.mp3',
+        ];
+
+        return $this->json($song);
+    }
+}
