afea0ca8a573c12f29d7d49e879ef0e8d220754e
Nicolasjmcrt <jumeaucourtn@gmail.com>
2023-01-24 15:17:39 +0100

    Chapter 7 : Twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,29 @@
 
 namespace App\Controller;
 
+use function Symfony\Component\String\u;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function Symfony\Component\String\u;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('Title: PB & Jams');
+
+        $tracks = [
+            ['song' => 'Smells Like Teen Spirit', 'artist' => 'Nirvana'],
+            ['song' => 'Loser', 'artist' => 'Beck'],
+            ['song' => 'Ironic', 'artist' => 'Alanis Morissette'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Linger', 'artist' => 'The Cranberries'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{slug}')]
